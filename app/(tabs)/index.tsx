71b318226d9ca513f2499d66b194618364e68bac
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2024-10-25 18:26:38 -0400

    update index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -24,7 +24,7 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">888Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.titleContainer}>
